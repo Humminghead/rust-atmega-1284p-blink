23bdaafa6f680c23a2a5e38a70889f4dd8ef7084
Vladimir P <vladimir_pogonin@hotmail.com>
2023-01-10 11:43:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# rust-atmega-1284p-blink
+atmega-1284p-blink
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+#![feature(lang_items)]
+
+// https://doc.rust-lang.org/error_codes/E0432.html
+extern crate panic_halt;
+
+// dev profile: easier to debug panics; can put a breakpoint on `rust_begin_unwind`
+#[cfg(debug_assertions)]
+use panic_halt as _;
+
+// release profile: minimize the binary size of the application
+#[cfg(not(debug_assertions))]
+use panic_abort as _;
+
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+
+#[no_mangle]
+pub extern fn main() {
+    loop {
+        // port::B5::set_high();
+        // ruduino::delay::delay_ms(1000);
+        // port::B5::set_low();
+        // ruduino::delay::delay_ms(1000);
+    }
+}
