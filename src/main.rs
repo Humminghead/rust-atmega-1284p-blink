41898b9241a002508a12e0162327c6b5871a61bb
Vladimir <vladimir_pogonin@hotmail.com>
2023-05-11 15:42:59 +0300

    Duplicate `panic_abort' was deleted

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,6 @@
 // https://doc.rust-lang.org/error_codes/E0432.html
 extern crate panic_halt;
 
-// dev profile: easier to debug panics; can put a breakpoint on `rust_begin_unwind`
-// #[cfg(debug_assertions)]
-// use panic_halt as _;
-
-// release profile: minimize the binary size of the application
-#[cfg(not(debug_assertions))]
-use panic_abort as _;
-
 #[lang = "eh_personality"]
 extern "C" fn eh_personality() {}
 
